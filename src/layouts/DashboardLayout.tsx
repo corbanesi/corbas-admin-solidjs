0db0cca601d6fb0cc9fd78631965ff5082cfa650
corbanesi <alessandrocorbanesi@gmail.com>
2022-09-22 21:35:14 -0300

    chore(alias): added alias for folders inside src

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,3 +1,4 @@
+import { NavBar } from "@components/NavBar";
 import { useAuthStateProvider } from "@providers/AuthProvider";
 import { Outlet, useNavigate } from "@solidjs/router";
 import { onMount, ParentComponent } from "solid-js";
@@ -13,8 +14,9 @@ const DashboardLayout: ParentComponent = (props) => {
   });
 
   return (
-    <div class="md:py-4">
+    <div class="h-full md:py-4">
       <div class="container mx-auto h-full rounded-lg border bg-slate-200/30 shadow-xl">
+        <NavBar />
         <Outlet />
       </div>
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import { Card } from "@components/Card";
-import { NavBar } from "@components/NavBar";
-import type { Component } from "solid-js";
+import { Component } from "solid-js";
 
 const HomePage: Component = () => {
   return (
     <>
-      <NavBar />
       <div class="flex flex-row flex-wrap justify-center gap-4">
         <Card />
         <Card />
