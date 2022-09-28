26ab3724b5855db8452f108a8af4bb450ce55292
corbanesi <alessandrocorbanesi@gmail.com>
2022-09-28 20:24:43 -0300

    fix(users-table): added error boundary for users table

--- src/components/atomic/corbas.user.table.tsx
+++ src/components/atomic/corbas.user.table.tsx
@@ -4,7 +4,7 @@ import {
   flexRender,
   getCoreRowModel,
 } from "@tanstack/solid-table";
-import { For, Suspense, VoidComponent } from "solid-js";
+import { ErrorBoundary, For, Suspense, VoidComponent } from "solid-js";
 import CorbasLoading from "./corbas.loading";
 
 const defaultColumns: ColumnDef<User>[] = [
@@ -49,51 +49,53 @@ const CorbasUserTable: VoidComponent<CorbasUserTableProps> = (props) => {
 
   return (
     <>
-      <div class="not-prose relative overflow-hidden rounded-xl bg-white/30 leading-5 shadow-xl shadow-black/5 ring-1  ring-slate-200">
+      <div class="not-prose relative overflow-hidden rounded-xl bg-white/30 leading-5 shadow-xl shadow-black/5 ring-1 ring-slate-200">
         <div class="relative overflow-auto rounded-xl">
           <div class="my-8 overflow-hidden">
-            <Suspense fallback={<CorbasLoading />}>
-              <table class="w-full table-auto border-collapse text-sm">
-                <thead>
-                  <For each={table.getHeaderGroups()}>
-                    {(headerGroup) => (
-                      <tr>
-                        <For each={headerGroup.headers}>
-                          {(header) => (
-                            <th class="border-b p-4 pl-8 pt-0 pb-3 text-left font-medium text-slate-900">
-                              {header.isPlaceholder
-                                ? null
-                                : flexRender(
+            <ErrorBoundary fallback={<div class="w-full flex justify-center text-slate-700">Error TODO</div>}>
+              <Suspense fallback={<CorbasLoading />}>
+                <table class="w-full table-auto border-collapse text-sm">
+                  <thead>
+                    <For each={table.getHeaderGroups()}>
+                      {(headerGroup) => (
+                        <tr>
+                          <For each={headerGroup.headers}>
+                            {(header) => (
+                              <th class="border-b p-4 pl-8 pt-0 pb-3 text-left font-medium text-slate-900">
+                                {header.isPlaceholder
+                                  ? null
+                                  : flexRender(
                                     header.column.columnDef.header,
                                     header.getContext()
                                   )}
-                            </th>
-                          )}
-                        </For>
-                      </tr>
-                    )}
-                  </For>
-                </thead>
-                <tbody class="bg-slate-200/30">
-                  <For each={table.getRowModel().rows}>
-                    {(row) => (
-                      <tr>
-                        <For each={row.getVisibleCells()}>
-                          {(cell) => (
-                            <td class="border-b border-slate-100 p-4 pl-8 text-slate-700">
-                              {flexRender(
-                                cell.column.columnDef.cell,
-                                cell.getContext()
-                              )}
-                            </td>
-                          )}
-                        </For>
-                      </tr>
-                    )}
-                  </For>
-                </tbody>
-              </table>
-            </Suspense>
+                              </th>
+                            )}
+                          </For>
+                        </tr>
+                      )}
+                    </For>
+                  </thead>
+                  <tbody class="bg-slate-200/30">
+                    <For each={table.getRowModel().rows}>
+                      {(row) => (
+                        <tr>
+                          <For each={row.getVisibleCells()}>
+                            {(cell) => (
+                              <td class="border-b border-slate-100 p-4 pl-8 text-slate-700">
+                                {flexRender(
+                                  cell.column.columnDef.cell,
+                                  cell.getContext()
+                                )}
+                              </td>
+                            )}
+                          </For>
+                        </tr>
+                      )}
+                    </For>
+                  </tbody>
+                </table>
+              </Suspense>
+            </ErrorBoundary>
           </div>
         </div>
       </div>
